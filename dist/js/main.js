54998e8070143a2379e2d14fddb4785f892a93a8
Suraj Baidya <suraj@bentraytech.com>
2024-04-17 19:59:15 +0545

    new release section done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,23 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".menu-btn");
+  const navLinks = document.querySelector(".nav-links");
 
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-  menuBtn.classList.toggle("cross");
+  if (menuBtn && navLinks) {
+    // Check if both elements are found
+    menuBtn.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+      menuBtn.classList.toggle("cross");
+    });
+  }
 });
+
+/* hover js */
+/* document.querySelectorAll(".news__book").forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    item.querySelector(".news__release").style.display = "block";
+  });
+
+  item.addEventListener("mouseleave", () => {
+    item.querySelector(".news__release").style.display = "none";
+  });
+}); */
